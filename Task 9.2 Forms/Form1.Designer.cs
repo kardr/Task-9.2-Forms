0ac8bbb3ba994e6b4e65541f3938d4de79d1edb7
Карина Дроздова <K.Drozd33@yandex.ru>
2020-05-22 14:07:47 +0300

    .

--- Task 9.2 Forms/Form1.Designer.cs
+++ Task 9.2 Forms/Form1.Designer.cs
@@ -28,13 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(186, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Переписать";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(490, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Переписать в новый файл все его строки, удалив из них символы, стоящие на четных " +
+    "местах.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(492, 257);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Task 9.2 Forms/Form1.cs
+++ Task 9.2 Forms/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Task_9._2_Forms
 {
@@ -15,6 +16,31 @@ namespace Task_9._2_Forms
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader("text.txt");
+                StreamWriter sw = new StreamWriter("text2.txt");
+                while (sr.EndOfStream != true)
+                {
+                    string s = sr.ReadLine();
+                    string s2 = "";
+                    for (int i = 1; i < s.Length; i += 2)
+                    {
+                        s2 += s[i];
+                    }
+                    sw.WriteLine(s2);
+                }
+                sr.Close();
+                sw.Close();
+            }
+            catch (Exception E)
+            {
+               MessageBox.Show(E.Message);
+            }
         }
     }
 }
